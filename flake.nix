907f5601c11af06f48deb7c742c8f2841535a9d8
ahirner <a.hirner@gmail.com>
2022-12-29 23:52:20 +0100

    add pytorch 1.13 with workarounds
    errors eventually with:libcudnn_ops_infer.so.8: cannot open shared object file: No such file or directory

--- flake.nix
+++ flake.nix
@@ -4,7 +4,8 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.poetry2nix = {
-    url = "github:nix-community/poetry2nix";
+    # includes torch recursive fix from: github:cpcloud/poetry2nix/rollup
+    url = "github:ahirner/poetry2nix/fix/wheel-recursion";
     inputs.nixpkgs.follows = "nixpkgs";
     inputs.flake-utils.follows = "flake-utils";
   };
@@ -16,38 +17,83 @@
     poetry2nix,
   }:
     {
-      # Nixpkgs overlay providing the application
       overlay = nixpkgs.lib.composeManyExtensions [
         poetry2nix.overlay
-        (final: prev: {
-          y-torch = prev.poetry2nix.mkPoetryApplication {
+        (final: pkgs: rec {
+          # cuda surgery: https://github.com/nix-community/poetry2nix/issues/850#issuecomment-1356844427
+          customOverrides = self: super: {
+            nvidia-cudnn-cu11 = super.nvidia-cudnn-cu11.overridePythonAttrs (attrs: {
+              nativeBuildInputs = attrs.nativeBuildInputs or [] ++ [pkgs.autoPatchelfHook];
+              preFixup = ''
+                addAutoPatchelfSearchPath "${self.nvidia-cublas-cu11}/${self.python.sitePackages}/nvidia/cublas/lib"
+              '';
+              postFixup = ''
+                rm -r $out/${self.python.sitePackages}/nvidia/{__pycache__,__init__.py}
+              '';
+              propagatedBuildInputs =
+                attrs.propagatedBuildInputs
+                or []
+                ++ [
+                  self.nvidia-cublas-cu11
+                ];
+            });
+            nvidia-cuda-nvrtc-cu11 = super.nvidia-cuda-nvrtc-cu11.overridePythonAttrs (_: {
+              postFixup = ''
+                rm -r $out/${self.python.sitePackages}/nvidia/{__pycache__,__init__.py}
+              '';
+            });
+            torch = super.torch.overridePythonAttrs (attrs: {
+              nativeBuildInputs =
+                attrs.nativeBuildInputs
+                or []
+                ++ [
+                  pkgs.autoPatchelfHook
+                  pkgs.cudaPackages.autoAddOpenGLRunpathHook
+                ];
+              buildInputs =
+                attrs.buildInputs
+                or []
+                ++ [
+                  self.nvidia-cudnn-cu11
+                  self.nvidia-cuda-nvrtc-cu11
+                  self.nvidia-cuda-runtime-cu11
+                ];
+              postInstall = ''
+                addAutoPatchelfSearchPath "${self.nvidia-cublas-cu11}/${self.python.sitePackages}/nvidia/cublas/lib"
+                addAutoPatchelfSearchPath "${self.nvidia-cudnn-cu11}/${self.python.sitePackages}/nvidia/cudnn/lib"
+                addAutoPatchelfSearchPath "${self.nvidia-cuda-nvrtc-cu11}/${self.python.sitePackages}/nvidia/cuda_nvrtc/lib"
+              '';
+            });
+          };
+
+          # application
+          y-torch = pkgs.poetry2nix.mkPoetryApplication {
             projectDir = ./.;
             preferWheels = true;
+            overrides = pkgs.poetry2nix.overrides.withDefaults customOverrides;
           };
+
+          # environment
+          env =
+            pkgs.poetry2nix.mkPoetryEnv
+            {
+              projectDir = ./.;
+              preferWheels = true;
+              overrides = pkgs.poetry2nix.overrides.withDefaults customOverrides;
+            };
         })
       ];
     }
-    // (flake-utils.lib.eachDefaultSystem (system: let
+    // flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {
         inherit system;
         overlays = [self.overlay];
-        config.allowUnfree = true;
       };
-
-      customOverrides = self: super: {};
-      env =
-        pkgs.poetry2nix.mkPoetryEnv
-        {
-          projectDir = ./.;
-          preferWheels = true;
-          overrides = pkgs.poetry2nix.overrides.withDefaults customOverrides;
-        };
     in {
       apps.default = {
         program = "${pkgs.y-torch}/bin/y-torch";
         type = "app";
       };
-
       packages.default = pkgs.poetry;
 
       devShell = pkgs.mkShell {
@@ -56,5 +102,5 @@
           env
         ];
       };
-    }));
+    });
 }
