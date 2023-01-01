0353720bc08c7b680b359c5301ea58ac20d2adbe
ahirner <a.hirner@gmail.com>
2023-01-01 21:09:44 +0100

    fix missing libcudnn_ops_infer.so.8, saner flake file

--- flake.nix
+++ flake.nix
@@ -4,8 +4,7 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.poetry2nix = {
-    # includes torch recursive fix from: github:cpcloud/poetry2nix/rollup
-    url = "github:ahirner/poetry2nix/fix/wheel-recursion";
+    url = "github:nix-community/poetry2nix";
     inputs.nixpkgs.follows = "nixpkgs";
     inputs.flake-utils.follows = "flake-utils";
   };
@@ -16,91 +15,86 @@
     flake-utils,
     poetry2nix,
   }:
-    {
-      overlay = nixpkgs.lib.composeManyExtensions [
-        poetry2nix.overlay
-        (final: pkgs: rec {
-          # cuda surgery: https://github.com/nix-community/poetry2nix/issues/850#issuecomment-1356844427
-          customOverrides = self: super: {
-            nvidia-cudnn-cu11 = super.nvidia-cudnn-cu11.overridePythonAttrs (attrs: {
-              nativeBuildInputs = attrs.nativeBuildInputs or [] ++ [pkgs.autoPatchelfHook];
-              preFixup = ''
-                addAutoPatchelfSearchPath "${self.nvidia-cublas-cu11}/${self.python.sitePackages}/nvidia/cublas/lib"
-              '';
-              postFixup = ''
-                rm -r $out/${self.python.sitePackages}/nvidia/{__pycache__,__init__.py}
-              '';
-              propagatedBuildInputs =
-                attrs.propagatedBuildInputs
-                or []
-                ++ [
-                  self.nvidia-cublas-cu11
-                ];
-            });
-            nvidia-cuda-nvrtc-cu11 = super.nvidia-cuda-nvrtc-cu11.overridePythonAttrs (_: {
-              postFixup = ''
-                rm -r $out/${self.python.sitePackages}/nvidia/{__pycache__,__init__.py}
-              '';
-            });
-            torch = super.torch.overridePythonAttrs (attrs: {
-              nativeBuildInputs =
-                attrs.nativeBuildInputs
-                or []
-                ++ [
-                  pkgs.autoPatchelfHook
-                  pkgs.cudaPackages.autoAddOpenGLRunpathHook
-                ];
-              buildInputs =
-                attrs.buildInputs
-                or []
-                ++ [
-                  self.nvidia-cudnn-cu11
-                  self.nvidia-cuda-nvrtc-cu11
-                  self.nvidia-cuda-runtime-cu11
-                ];
-              postInstall = ''
-                addAutoPatchelfSearchPath "${self.nvidia-cublas-cu11}/${self.python.sitePackages}/nvidia/cublas/lib"
-                addAutoPatchelfSearchPath "${self.nvidia-cudnn-cu11}/${self.python.sitePackages}/nvidia/cudnn/lib"
-                addAutoPatchelfSearchPath "${self.nvidia-cuda-nvrtc-cu11}/${self.python.sitePackages}/nvidia/cuda_nvrtc/lib"
-              '';
-            });
-          };
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [poetry2nix.overlay];
+          config.allowUnfree = true;
+        };
 
-          # application
-          y-torch = pkgs.poetry2nix.mkPoetryApplication {
-            projectDir = ./.;
-            preferWheels = true;
-            overrides = pkgs.poetry2nix.overrides.withDefaults customOverrides;
-          };
+        # cuda surgery: https://github.com/cpcloud/torch-p2nix
+        customOverrides = self: super: {
+          # can be removed after https://github.com/nix-community/poetry2nix/issues/850
+          wheel = super.wheel.override {preferWheel = false;};
 
-          # environment
-          env =
-            pkgs.poetry2nix.mkPoetryEnv
-            {
-              projectDir = ./.;
-              preferWheels = true;
-              overrides = pkgs.poetry2nix.overrides.withDefaults customOverrides;
-            };
-        })
-      ];
-    }
-    // flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [self.overlay];
-      };
-    in {
-      apps.default = {
-        program = "${pkgs.y-torch}/bin/y-torch";
-        type = "app";
-      };
-      packages.default = pkgs.poetry;
+          nvidia-cudnn-cu11 = super.nvidia-cudnn-cu11.overridePythonAttrs (attrs: {
+            nativeBuildInputs = attrs.nativeBuildInputs or [] ++ [pkgs.autoPatchelfHook];
+            propagatedBuildInputs =
+              attrs.propagatedBuildInputs
+              or []
+              ++ [
+                self.nvidia-cublas-cu11
+                self.pkgs.cudaPackages.cudnn_8_5_0
+              ];
 
-      devShell = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          poetry
-          env
-        ];
-      };
-    });
+            preFixup = ''
+              addAutoPatchelfSearchPath "${self.nvidia-cublas-cu11}/${self.python.sitePackages}/nvidia/cublas/lib"
+            '';
+            postFixup = ''
+              rm -r $out/${self.python.sitePackages}/nvidia/{__pycache__,__init__.py}
+            '';
+          });
+
+          nvidia-cuda-nvrtc-cu11 = super.nvidia-cuda-nvrtc-cu11.overridePythonAttrs (_: {
+            postFixup = ''
+              rm -r $out/${self.python.sitePackages}/nvidia/{__pycache__,__init__.py}
+            '';
+          });
+
+          torch = super.torch.overridePythonAttrs (attrs: {
+            nativeBuildInputs =
+              attrs.nativeBuildInputs
+              or []
+              ++ [
+                pkgs.autoPatchelfHook
+                pkgs.cudaPackages.autoAddOpenGLRunpathHook
+              ];
+            buildInputs =
+              attrs.buildInputs
+              or []
+              ++ [
+                self.nvidia-cudnn-cu11
+                self.nvidia-cuda-nvrtc-cu11
+                self.nvidia-cuda-runtime-cu11
+              ];
+            postInstall = ''
+              addAutoPatchelfSearchPath "${self.nvidia-cublas-cu11}/${self.python.sitePackages}/nvidia/cublas/lib"
+              addAutoPatchelfSearchPath "${self.nvidia-cudnn-cu11}/${self.python.sitePackages}/nvidia/cudnn/lib"
+              addAutoPatchelfSearchPath "${self.nvidia-cuda-nvrtc-cu11}/${self.python.sitePackages}/nvidia/cuda_nvrtc/lib"
+            '';
+          });
+        };
+
+        y-torch = pkgs.poetry2nix.mkPoetryApplication {
+          projectDir = ./.;
+          preferWheels = true;
+          overrides = [customOverrides pkgs.poetry2nix.defaultPoetryOverrides];
+          python = pkgs.python310;
+        };
+        env = pkgs.poetry2nix.mkPoetryEnv {
+          projectDir = ./.;
+          preferWheels = true;
+          overrides = [customOverrides pkgs.poetry2nix.defaultPoetryOverrides];
+          python = pkgs.python310;
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [pkgs.poetry env];
+        };
+        packages.env = env;
+        packages.default = y-torch;
+        packages.poetry = pkgs.poetry;
+      }
+    );
 }
